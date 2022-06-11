d8180eb8ab54e5f51f84d662930fb6719aab74ca
Akshay Chand <106740813+RoyalAkshay@users.noreply.github.com>
2022-06-11 20:06:16 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/BalamuruganDV/LUNA-EXTRA-FEATURES.git /LUNA-EXTRA-FEATURES
+  git clone https://github.com/RoyalAkshay/LUNA-EXTRA-FEATURES.git /LUNA-EXTRA-FEATURES
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /LUNA-EXTRA-FEATURES
