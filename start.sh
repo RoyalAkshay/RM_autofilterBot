2428b47ec8cfc6bbb1d54834b3e4943b43db2081
Akshay Chand <106740813+RoyalAkshay@users.noreply.github.com>
2022-06-13 19:16:49 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,9 +4,9 @@ then
   git clone https://github.com/RoyalAkshay/LUNA-EXTRA-FEATURES.git /LUNA-EXTRA-FEATURES
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /LUNA-EXTRA-FEATURES
+  git clone $UPSTREAM_REPO /RM_autofilterBot
 fi
 cd /LUNA-EXTRA-FEATURES
 pip3 install -U -r requirements.txt
-echo "Starting Rashmika Bot..."
+echo "Starting Rashmika Bot ☺️☺️..."
 python3 bot.py
